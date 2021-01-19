369255d817ffad9458d61c449b8c37b64b28e511
GamingGuy003 <gamingguy003@fedora.email>
2021-01-19 16:44:42 +0100

    Added instructions 0x20 - 0x2F

--- Z80/iManager/iSet/MAINIS/mainIs.cpp
+++ Z80/iManager/iSet/MAINIS/mainIs.cpp
@@ -41,8 +41,8 @@ void MainIS::exec(uint8_t* is){
         case 0x14:  z->D(idas->inc(z->D()));                                                            break;  //INC D
         case 0x15:  z->D(idas->dec(z->D()));                                                            break;  //DEC D
         case 0x16:  z->D(is[1]);                                                                        break;  //LD D, *
-        case 0x17:  log->logUnimplemented(op);                                                          break;  //RLCA              NOT IMPLEMENTED YET!
-        case 0x18:  log->logUnimplemented(op);                                                          break;  //EX AF, AF'        NOT IMPLEMENTED YET!
+        case 0x17:  log->logUnimplemented(op);                                                          break;  //RLA               NOT IMPLEMENTED YET!
+        case 0x18:  log->logUnimplemented(op);                                                          break;  //JR *              NOT IMPLEMENTED YET!
         case 0x19:  z->HL(idas->addX16(z->HL(), z->DE()));                                              break;  //ADD HL, DE
         case 0x1A:  z->A(z->mM->get(z->DE()));                                                          break;  //LD A, (DE)
         case 0x1B:  z->DE(idas->decX16(z->DE()));                                                       break;  //DEC DE
@@ -50,6 +50,23 @@ void MainIS::exec(uint8_t* is){
         case 0x1D:  z->E(idas->dec(z->E()));                                                            break;  //DEC E
         case 0x1E:  z->E(is[1]);                                                                        break;  //LD E, *
         case 0x1F:  log->logUnimplemented(op);                                                          break;  //RRA              NOT IMPLEMENTED YET!
+        
+        case 0x20:  log->logUnimplemented(op);                                                          break;  //JR NZ; *         NOT IMPLEMENTED YET!
+        case 0x21:  z->HL(z->getX16(is[1], is[2]));                                                     break;  //LD HL, **
+        case 0x22:  z->mM->setX16(z->getX16(is[1], is[2]), z->HL());                                    break;  //LD (**), HL
+        case 0x23:  z->HL(idas->incX16(z->HL()));                                                       break;  //INC HL
+        case 0x24:  z->H(idas->inc(z->H()));                                                            break;  //INC H
+        case 0x25:  z->H(idas->dec(z->H()));                                                            break;  //DEC H
+        case 0x26:  z->H(is[1]);                                                                        break;  //LD H, *
+        case 0x27:  log->logUnimplemented(op);                                                          break;  //DAA              NOT IMPLEMENTED YET!
+        case 0x28:  log->logUnimplemented(op);                                                          break;  //JR Z, *          NOT IMPLEMENTED YET!
+        case 0x29:  z->HL(idas->addX16(z->HL(), z->HL()));                                              break;  //ADD HL, HL
+        case 0x2A:  z->HL(z->mM->getX16(z->getX16(is[1], is[2])));                                      break;  //LD HL, (**)
+        case 0x2B:  z->DE(idas->decX16(z->DE()));                                                       break;  //DEC DE
+        case 0x2C:  z->E(idas->inc(z->E()));                                                            break;  //INC E
+        case 0x2D:  z->E(idas->dec(z->E()));                                                            break;  //DEC E
+        case 0x2E:  z->E(is[1]);                                                                        break;  //LD E, *
+        case 0x2F:  log->logUnimplemented(op);                                                          break;  //RRA              NOT IMPLEMENTED YET!
         
         
         
