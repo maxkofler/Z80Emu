190e261923feb44400f62a1cf9dc9549d8f6fa65
Max Kofler <kofler.max.dev@gmail.com>
2021-01-20 09:38:37 +0100

    Finsished works on rotation instructions

--- Z80/iManager/iSet/rotate/rotate.cpp
+++ Z80/iManager/iSet/rotate/rotate.cpp
@@ -9,7 +9,7 @@ uint8_t Rotate::RLC(uint8_t reg){
     this->z80->NF(false);
     this->z80->HF(false);
     this->z80->CF(reg >> 7);
-    return reg << 1;
+    return (reg << 1) | (this->z80->CF() << 0);
 }
 
 uint8_t Rotate::RL(uint8_t reg){
@@ -17,7 +17,7 @@ uint8_t Rotate::RL(uint8_t reg){
     bool C;
     uint8_t nreg = reg << 1;
     C = (nreg >> 7);
-    nreg = nreg & (this->z80->CF() << 0);
+    nreg = nreg | (this->z80->CF() << 0);
     this->z80->NF(false);
     this->z80->HF(false);
     this->z80->CF(C);
@@ -30,7 +30,7 @@ uint8_t Rotate::RRC(uint8_t reg){
     this->z80->NF(false);
     this->z80->HF(false);
     this->z80->CF(reg);
-    return reg >> 1;
+    return (reg >> 1) | (this->z80->CF() << 7);
 }
 
 uint8_t Rotate::RR(uint8_t reg){
@@ -38,7 +38,7 @@ uint8_t Rotate::RR(uint8_t reg){
     bool C;
     uint8_t nreg = reg >> 1;
     C = nreg;
-    nreg = nreg & (this->z80->CF() << 7);
+    nreg = nreg | (this->z80->CF() << 7);
     this->z80->NF(false);
     this->z80->HF(false);
     this->z80->CF(C);
