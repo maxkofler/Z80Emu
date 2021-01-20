6382ebd179a17da2dc230824d5d0f83a666e4edc
Max Kofler <kofler.max.dev@gmail.com>
2021-01-20 13:47:43 +0100

    Added prototypes for AND, OR, XOR

---
+++ Z80/iManager/iSet/arithmetic/arithmetic.cpp
@@ -0,0 +1,17 @@
+#include "arithmetic.h"
+
+Arithmetic::Arithmetic(Z80* z80){
+    this->z80 = z80;
+}
+
+uint8_t Arithmetic::AND(uint8_t reg1, uint8_t reg2){
+    return 0;
+}
+
+uint8_t Arithmetic::OR(uint8_t reg1, uint8_t reg2){
+    return 0;
+}
+
+uint8_t Arithmetic::XOR(uint8_t reg1, uint8_t reg2){
+    return 0;
+}
---
+++ Z80/iManager/iSet/arithmetic/arithmetic.h
@@ -0,0 +1,21 @@
+#ifndef __ARITHMETIC_H__
+#define __ARITHMETIC_H__
+
+class Arithmetic;
+#include "../../../../z80.h"
+#include <stdint.h>
+
+class Arithmetic{
+public:
+    Arithmetic(Z80*);
+
+    uint8_t AND(uint8_t, uint8_t);
+    uint8_t OR(uint8_t, uint8_t);
+
+    uint8_t XOR(uint8_t, uint8_t);
+
+private:
+    Z80* z80;
+};
+
+#endif
