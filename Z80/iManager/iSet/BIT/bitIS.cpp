9ccdd40825f893cb8d7fb160299490eece1cf336
GamingGuy003 <gamingguy003@fedora.email>
2021-01-20 22:18:34 +0100

    Implemented BitInstructions 0x00 - 0x0F

--- Z80/iManager/iSet/BIT/bitIS.cpp
+++ Z80/iManager/iSet/BIT/bitIS.cpp
@@ -12,8 +12,20 @@ void BitIS::exec(uint8_t op){
     switch(op){
         case 0x00:  z->B(rot->RLC(z->B()));                                                             break;  //RLC B
         case 0x01:  z->C(rot->RLC(z->C()));                                                             break;  //RLC C
-        //
+        case 0x02:  z->D(rot->RLC(z->D()));                                                             break;  //RLC D
+        case 0x03:  z->E(rot->RLC(z->E()));                                                             break;  //RLC E
+        case 0x04:  z->H(rot->RLC(z->H()));                                                             break;  //RLC H
+        case 0x05:  z->L(rot->RLC(z->L()));                                                             break;  //RLC L
+        case 0x06:  z->mM->set(z->HL(), rot->RLC(z->mM->get(z->HL())));                                 break;  //RLC (HL)
+        case 0x07:  z->A(rot->RLC(z->A()));                                                             break;  //RLC A
         case 0x08:  z->B(rot->RRC(z->B()));                                                             break;  //RRC B
+        case 0x09:  z->C(rot->RRC(z->B()));                                                             break;  //RRC C
+        case 0x0A:  z->D(rot->RRC(z->B()));                                                             break;  //RRC D
+        case 0x0B:  z->E(rot->RRC(z->B()));                                                             break;  //RRC E
+        case 0x0C:  z->H(rot->RRC(z->B()));                                                             break;  //RRC H
+        case 0x0D:  z->L(rot->RRC(z->B()));                                                             break;  //RRC L
+        case 0x0E:  z->mM->set(z->HL(), rot->RRC(z->mM->get(z->HL())));                                 break;  //RRC (HL)
+        case 0x0F:  z->A(rot->RRC(z->B()));                                                             break;  //RRC A
 
         case 0x10:  z->B(rot->RL(z->B()));                                                              break;  //RL B
         //
