c8d8ae92754f4fa40a972c4f628799fbcbba155f
Max Kofler <kofler.max.dev@gmail.com>
2021-01-20 16:00:38 +0100

    Added CPUctl function prototypes: -jp -jr
    -call
    -ret
    
    -push
    -pop
    
    -rst
    
    -ex
    -exx

--- Z80/iManager/iSet/cpuctl/cpuctl.cpp
+++ Z80/iManager/iSet/cpuctl/cpuctl.cpp
@@ -3,3 +3,35 @@
 CPUctl::CPUctl(Z80* z80){
     this->z80 = z80;
 }
+
+uint16_t    CPUctl::jp      (bool c, uint16_t pos){
+    return 0;
+}
+uint16_t    CPUctl::jr      (bool c, uint8_t off){
+    return 0;
+}
+
+uint16_t    CPUctl::call    (bool c, uint16_t pos){
+    return 0;
+}
+uint16_t    CPUctl::ret     (bool c){
+    return 0;
+}
+
+void        CPUctl::push    (uint16_t v){
+
+}
+uint16_t    CPUctl::pop     (){
+    return 0;
+}
+
+void        CPUctl::rst     (uint16_t nv){
+
+}
+
+void        CPUctl::ex      (uint16_t* reg1, uint16_t* reg2){
+
+}
+void        CPUctl::exx     (){
+
+}
--- Z80/iManager/iSet/cpuctl/cpuctl.h
+++ Z80/iManager/iSet/cpuctl/cpuctl.h
@@ -10,6 +10,20 @@ class CPUctl{
 public:
     CPUctl(Z80*);
 
+    uint16_t    jp      (bool condition, uint16_t position);
+    uint16_t    jr      (bool condition, uint8_t offset);
+
+    uint16_t    call    (bool condition, uint16_t position);
+    uint16_t    ret     (bool condition);
+
+    void        push    (uint16_t value);
+    uint16_t    pop     ();
+
+    void        rst     (uint16_t new_value);
+
+    void        ex      (uint16_t* reg1, uint16_t* reg2);
+    void        exx     ();
+
 private:
     Z80* z80;
 };
