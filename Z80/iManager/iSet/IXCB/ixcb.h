e9b6a4d3118c55cc7513326f62edc1ad02d7d7ed
Max Kofler <kofler.max.dev@gmail.com>
2021-01-21 11:52:40 +0100

    Begin works on implementing IXCB instructions

---
+++ Z80/iManager/iSet/IXCB/ixcb.h
@@ -0,0 +1,30 @@
+#ifndef __IXCB_H__
+#define __IXCB_H__
+
+class IXCB;
+
+#include "../../../log/log.h"
+#include "../../../../z80.h"
+
+#include "../rotate/rotate.h"
+#include "../bit/bit.h"
+
+class IXCB{
+
+public:
+    IXCB(Z80* z80);
+
+    void exec(uint8_t* opcode);          //These instructions don't have any operands!
+
+    uint8_t getCycles(uint8_t);
+
+private:
+    Z80* z;
+    Log* log;
+
+    Rotate* rot;
+    Bit* bit;
+
+};
+
+#endif
