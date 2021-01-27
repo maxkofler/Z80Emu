065abda1dab1efc3c45f00cfb8ae8555abd901c7
Max Kofler <kofler.max.dev@gmail.com>
2021-01-27 12:15:35 +0100

    Implemented all Bit instructions

--- Z80/iManager/iSet/bit/bit.cpp
+++ Z80/iManager/iSet/bit/bit.cpp
@@ -5,10 +5,17 @@ Bit::Bit(Z80* z80){
 }
 
 void Bit::bit(uint8_t pos, uint8_t reg){
-
+    //Flags:    N(r), H(s), Z(d)
+    bool r = (reg >> pos) & 1;
+    z80->NF(false);
+    z80->HF(true);
+    z80->ZF(!r);
 }
 void Bit::bit(uint8_t pos, uint16_t reg){
-    
+    bool r = (reg >> pos) & 1;
+    z80->NF(false);
+    z80->HF(true);
+    z80->ZF(!r);
 }
 
 uint8_t Bit::res(uint8_t pos, uint8_t reg){
