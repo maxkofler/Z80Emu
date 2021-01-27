a0bf582721d5821bd914dc901795cdf0d2182a21
Max Kofler <kofler.max.dev@gmail.com>
2021-01-27 11:54:01 +0100

    Added function description to Bit instructions

--- Z80/iManager/iSet/bit/bit.h
+++ Z80/iManager/iSet/bit/bit.h
@@ -10,12 +10,15 @@ class Bit{
 public:
     Bit(Z80* z80);
 
+    //Tests bit (pos) of (reg), sets Z flag if bit is not set
     void bit(uint8_t pos, uint8_t reg);
     void bit(uint8_t pos, uint16_t reg);
 
+    //Resets bit (pos) of (reg)
     uint8_t res(uint8_t pos, uint8_t reg);
     uint16_t res(uint8_t pos, uint16_t reg);
 
+    //Sets bit (pos) of (reg)
     uint8_t set(uint8_t pos, uint8_t reg);
     uint16_t set(uint8_t pos, uint16_t reg);
 
