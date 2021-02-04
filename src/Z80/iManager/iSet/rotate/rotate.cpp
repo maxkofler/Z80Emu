762809715373844569dffa1aad2f4dce7b9ac7cc
Max Kofler <kofler.max.dev@gmail.com>
2021-02-04 17:25:19 +0100

    Refactored file structure

--- src/Z80/iManager/iSet/rotate/rotate.cpp
+++ src/Z80/iManager/iSet/rotate/rotate.cpp
@@ -13,10 +13,10 @@ uint8_t Rotate::RLC(uint8_t reg){
 }
 
 uint8_t Rotate::RL(uint8_t reg){
-    //Flags: N & H - reset, C
+    //Flags:    N(r),   H(r),   C(d)
     bool C;
     uint8_t nreg = reg << 1;
-    C = (nreg >> 7);
+    C = (reg >> 7);
     nreg = nreg | (this->z80->CF() << 0);
     this->z80->NF(false);
     this->z80->HF(false);
@@ -37,7 +37,7 @@ uint8_t Rotate::RR(uint8_t reg){
     //Flags: N & H - reset, C
     bool C;
     uint8_t nreg = reg >> 1;
-    C = nreg;
+    C = reg;
     nreg = nreg | (this->z80->CF() << 7);
     this->z80->NF(false);
     this->z80->HF(false);
--- src/Z80/iManager/iSet/rotate/rotate.h
+++ src/Z80/iManager/iSet/rotate/rotate.h
@@ -9,10 +9,14 @@ class Rotate{
 public:
     Rotate(Z80*);
 
+    //Rotates left 1 bit, bit 7 will be copied to carry and bit 0
     uint8_t RLC(uint8_t);
+    //Rotates left 1 bit, carry will be copied to bit 0 and bit 7 into carry
     uint8_t RL(uint8_t);
 
+    //Rotates right 1 bit, bit 0 will be copied to carry and bit 7
     uint8_t RRC(uint8_t);
+    //Rotates right 1 bit, carry will be copied to bit 7 and bit 0 to carry
     uint8_t RR(uint8_t);
 
     uint8_t     SRA     (uint8_t reg);
