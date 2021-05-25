ae1f3e5c93de684f483afeda15fe28254b3bd695
Max Kofler <kofler.max.dev@gmail.com>
2021-05-25 18:46:22 +0200

    Changed MemoryManager plotting

--- src/Z80/memoryManager/memoryManager_get.cpp
+++ src/Z80/memoryManager/memoryManager_get.cpp
@@ -2,13 +2,11 @@
 
 uint8_t MemoryManager::get(uint16_t pos){
     FUN();
-    LOGMEM("Read " + Log::toHexString(pos) + " -> [" + Log::toHexString(this->memory[pos]) + "]");
+    LOGMEM("MEM [" + Log::toHexString(pos) + "] > " + Log::toHexString(this->memory[pos]));
     return this->memory[pos];
 }
 
 uint16_t MemoryManager::getX16(uint16_t pos){
     FUN();
-    LOGMEM( "Read (16) " + Log::toHexString(pos) + " -> [" + 
-            Log::toHexString(this->memory[pos]) + "-" + Log::toHexString(this->memory[pos+1]) + "]");
     return (get(pos+1) << 8 ) | (get(pos) & 0xff);
 }
--- src/Z80/memoryManager/memoryManager_set.cpp
+++ src/Z80/memoryManager/memoryManager_set.cpp
@@ -12,25 +12,22 @@ bool MemoryManager::setX16(uint16_t pos, uint16_t val){
             LOGW(   "Denied write to ROM at pos: " + Log::toHexString(pos) + " | " + 
                     Log::toHexString(low) + " -> [" + Log::toHexString(memory[pos]) + "]");
         }else{
-            LOGMEM( "Writing(" + Log::toHexString(pos) + "): " + Log::toHexString(low) + " -> [" + 
-                    Log::toHexString(memory[pos]) + "]");
+            LOGMEM( "MEM [" + Log::toHexString(pos) + "] < " + Log::toHexString(low) + " old: " + Log::toHexString(memory[pos]));
             this->memory[pos] = low;
         }
 
         if (pos+1 >= this->lROM && pos+1 <= this->hROM){
             ret = false;
             LOGW(   "Denied write to ROM at pos: " + Log::toHexString(pos+1) + " | " + 
-                    Log::toHexString(low) + " -> [" + Log::toHexString(memory[pos+1]) + "]");
+                    Log::toHexString(high) + " -> [" + Log::toHexString(memory[pos+1]) + "]");
         }else{
-            LOGMEM( "Writing(" + Log::toHexString(pos+1) + "): " + Log::toHexString(high) + " -> [" +
-                    Log::toHexString(memory[pos+1]) + "]");
+            LOGMEM( "MEM [" + Log::toHexString(pos+1) + "] < " + Log::toHexString(high) + " old: " + Log::toHexString(memory[pos+1]));
             this->memory[pos+1] = high;
         }
         return ret;
     }
 
-    LOGMEM( "Writing(" + Log::toHexString(pos) + " - 16): " + Log::toHexString(val) + " -> [" + 
-            Log::toHexString(memory[pos]) + "]");
+    LOGMEM( "MEM [" + Log::toHexString(pos) + "] < " + Log::toHexString(low) + " old: " + Log::toHexString(memory[pos]));
     this->memory[pos+1] = val >> 8;     // high byte
     this->memory[pos] = val & 0x00FF;   // low byte (0x34)
     return true;
@@ -45,8 +42,7 @@ bool MemoryManager::set(uint16_t pos, uint8_t val){
             return false;
         }
     }
-    LOGMEM( "Writing(" + Log::toHexString(pos) + "): " + Log::toHexString(val) + " -> [" + 
-            Log::toHexString(memory[pos]) + "]");
+    LOGMEM( "MEM [" + Log::toHexString(pos) + "] < " + Log::toHexString(val) + " old: " + Log::toHexString(memory[pos]));
     this->memory[pos] = val;
     return true;
 }
